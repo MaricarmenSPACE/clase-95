4f815bd564f590feb95f8c47b2a5af0cc10975c6
MaricarmenSPACE <90126400+MaricarmenSPACE@users.noreply.github.com>
2022-10-19 19:02:52 -0500

    Add files via upload

---
+++ screens/CreateStory.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class CreateStory extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Crear un historia para mis alumnos</Text>
+      </View>
+    )
+  }
+}
